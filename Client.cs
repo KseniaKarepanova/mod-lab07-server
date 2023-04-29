0550260f8afa91095ae4370a5f49185cccc5e845
KseniaKarepanova <126484812+KseniaKarepanova@users.noreply.github.com>
2023-04-29 22:40:05 +0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07
+{
+    class Client
+    {
+        public event EventHandler<ProcEventArgs> request;
+        Server server;
+        public Client(Server server)
+        {
+            this.server = server;
+            this.request += server.proc;
+        }
+        protected virtual void OnProc(ProcEventArgs e)
+        {
+            EventHandler<ProcEventArgs> handler = request;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        public void Run(int id)
+        {
+            ProcEventArgs e = new ProcEventArgs();
+            e.id = id;
+            OnProc(e);
+        }
+
+    }
+}

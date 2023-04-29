8190483374882aca725b7891afdd09605ab3fcf9
KseniaKarepanova <kseniaserkar@gmail.com>
2023-04-29 22:29:15 +0300

    Initial commit

---
+++ Lab07/ProcEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07
+{
+    public class ProcEventArgs : EventArgs
+    {
+        public int id { get; set; }
+    }
+}
